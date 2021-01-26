19e9d0cd4b3de65636c83df418b834b3d96050b4
Alice <coisne.alice@gmail.com>
2021-01-26 18:56:14 +0100

    ajout "notre vision" page accueil + products.js avancée

--- js/products.js
+++ js/products.js
@@ -5,38 +5,41 @@
     console.log(id); //variable
     
 
-    const teddy = document.getElementById('teddy')
+    const teddy = document.getElementById('teddy');
+    const name = document.getElementById('teddy_name');
+    const img = document.getElementById('teddy_img');
+    const description = document.getElementById('teddy_description');
+    const price = document.getElementById('teddy_price');
+    const link = document.getElementById('link_basket');
+    let result;
+    
+
     const request = new XMLHttpRequest(); 
     request.open('GET', 'http://localhost:3000/api/teddies/' + id,true); //('bonjour' + teddy + ' !') = concaténation ajout de chaines de caractères ensemble
     request.send(); 
+    
 
     request.onreadystatechange = () => { //prêt quand l'état changera, tu exécuteras la suite
         if (request.readyState == 4 && request.status == 200) { /*readyState == 4 request done && status == 200 == request réussie*/
-            const results = JSON.parse(request.responseText); /*JSON.parse transforme les chaines de caractères en objet JSON*/
+            result = JSON.parse(request.responseText); /*JSON.parse transforme les chaines de caractères en objet JSON*/
 
-                // let name = teddy.name;
-                // let description = teddy.description;
-                // let img = teddy.imageUrl;
-                // let id = teddy._id;
-                //let price = teddy.price
+                img.setAttribute('src', result.imageUrl);
+                img.setAttribute('alt', result.name);
 
-                teddy.innerHTML = `
-                <div class="row">
-                <div class="col-md-6 photo-product text-center">
-                    <img src="${teddy.imageUrl}" alt="">
-                </div>
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">${teddy.name}</h5>
-                            <p class="card-text">${teddy.description}</p>
-                            <p class="card-text">${teddy.price}</p>
-                            <a href="./basket.html" class="btn btn-primary">Ajouter au panier</a>
-                        </div>
-                    </div>
-                </div>
-                </div>
-                `
+                teddy_name.innerText = result.name;
+                teddy_description.innerText = result.description;
+                teddy_price.innerText = (result.price/100 + ' €');
+
         }
     }
+
+    link.addEventListener('click',() => { //permet d'ajouter un évènement, ici au click sur le panier
+
+        let basket = localStorage.getItem('basket')  //getItem = permet de récupérer qqch du localStorage
+
+        if (basket === null) {
+           basket = []
+        }
+        console.log(basket)
+    } ) 
 })()
