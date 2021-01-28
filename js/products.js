6627b6cef636606d6d6ed325d8dae036651ccdc3
Alice <coisne.alice@gmail.com>
2021-01-28 15:29:48 +0100

    ajout panier

--- js/products.js
+++ js/products.js
@@ -19,6 +19,7 @@ function setTeddy(id) {
             let teddy = JSON.parse(request.responseText);
 
             showTeddy(teddy);
+            addBasket(teddy)
         }
     }
 
@@ -27,10 +28,8 @@ function setTeddy(id) {
 }
 
 function showTeddy(teddy) {
-    console.log(teddy)
-    const html = document.getElementById('teddy');
     const img = document.getElementById('teddy_img');
-    const link = document.getElementById('link_basket');
+    
     img.setAttribute('src', teddy.imageUrl);
     img.setAttribute('alt', teddy.name);
 
@@ -41,22 +40,26 @@ function showTeddy(teddy) {
 }
 
 function colors(colors) {
-    console.log(select_colors);//id du select
+
     colors.forEach(color => {
         select_colors.innerHTML += `
         <option value="${color}"> ${color} </option>`
-        
     })
-    console.log(colors)
 }
 
-function addBasket(basket) {
+function addBasket(teddy) {
+    
+    const link = document.getElementById('link_basket');
     link.addEventListener('click', () => { //permet d'ajouter un évènement, ici au click sur le panier
 
-        let basket = localStorage.getItem('basket')  //getItem = permet de récupérer qqch du localStorage
+        let basket = JSON.parse(localStorage.getItem('session_basket'))  //getItem = permet de récupérer qqch du localStorage
 
         if (basket === null) {
             basket = []
         }
+        console.log(typeof basket)
+        basket.push(teddy) //mettre danns le tableau
+
+        localStorage.setItem('session_basket', basket)
     })
 }
